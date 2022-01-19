029210c067f280faaabe60393b9e35c266bc1800
Joseph Ivie <joseph@lightningkite.com>
2022-01-19 15:17:12 -0700

    Moved repo to organization

--- makelinks.sh
+++ makelinks.sh
@@ -1,8 +1,9 @@
 mkdir demo/node_modules
-mkdir demo/node_modules/rxjs-plus
-rsync package.json demo/node_modules/rxjs-plus/package.json
-rsync jsx-runtime.js demo/node_modules/rxjs-plus/jsx-runtime.js
-rsync jsx-runtime.d.ts demo/node_modules/rxjs-plus/jsx-runtime.d.ts
-rsync index.js demo/node_modules/rxjs-plus/index.js
-rsync index.d.ts demo/node_modules/rxjs-plus/index.d.ts
-rsync -r --delete dist/ demo/node_modules/rxjs-plus/dist/
+mkdir demo/node_modules/@lightningkite
+mkdir demo/node_modules/@lightningkite/rxjs-plus
+rsync package.json demo/node_modules/@lightningkite/rxjs-plus/package.json
+rsync jsx-runtime.js demo/node_modules/@lightningkite/rxjs-plus/jsx-runtime.js
+rsync jsx-runtime.d.ts demo/node_modules/@lightningkite/rxjs-plus/jsx-runtime.d.ts
+rsync index.js demo/node_modules/@lightningkite/rxjs-plus/index.js
+rsync index.d.ts demo/node_modules/@lightningkite/rxjs-plus/index.d.ts
+rsync -r --delete dist/ demo/node_modules/@lightningkite/rxjs-plus/dist/
